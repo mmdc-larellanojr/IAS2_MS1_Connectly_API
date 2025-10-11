9a5e1d412fcb91a693318707874358df8ecd6a28
lmarellanojr.bsit <lmarellanojr.bsit@gmail.com>
2025-10-12 00:16:20 +0800

    feat: redirect root to /api/

--- Backend/connectly_backend/urls.py
+++ Backend/connectly_backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
